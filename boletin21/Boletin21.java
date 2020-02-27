7f1c1a73f5a049df29c0154d17fa990b8b631b1b
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2020-02-27 12:30:15 +0100

    boletin21

---
+++ boletin21/Boletin21.java
@@ -0,0 +1,41 @@
+
+package boletin21;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author DANIELCASTELAO\oyagualendara
+ */
+public class Boletin21 {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+          ArrayList<Libros> listaLibros = new ArrayList();
+    int opcion;
+    Metodos obx = new Metodos();
+    do {
+    opcion = Integer.parseInt(JOptionPane.showInputDialog("***MENU****\n 1 --> Engadir Libro\n 2 --> Amosar Libros\n 3 --> Borrar Libros\n 4 --> Buscar Libros\n 5 --> Modificar Libros"));
+    switch (opcion) {
+        case 1: 
+            listaLibros.add(obx.crearLibros());
+            break;
+        case 2:
+            obx.amosar(listaLibros);
+            break;
+        case 3:
+            obx.borrarLibros(listaLibros);
+            break;
+        case 4:
+            obx.buscarLibros(listaLibros);
+            break;
+        case 5:
+            obx.modificarLibros(listaLibros);
+            break;
+    }
+    } while (opcion != 6);
+    }
+}
+            
+    
+    
---
+++ boletin21/PedirDatos.java
@@ -0,0 +1,25 @@
+
+package boletin21;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author DANIELCASTELAO\oyagualendara
+ */
+public class PedirDatos {
+public static int pedirInt(){
+        return Integer.parseInt(JOptionPane.showInputDialog("Teclea Int:"));
+    }
+    public static int pedirInt(String mensaxe){
+        return Integer.parseInt(JOptionPane.showInputDialog(mensaxe));
+    }
+    
+    public static String pedirString(){
+        return JOptionPane.showInputDialog("Teclea String: ");
+    }
+     
+    public static String pedirString(String mensaxe){
+        return JOptionPane.showInputDialog(mensaxe);
+    }
+}
